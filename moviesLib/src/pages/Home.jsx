a2c73ddc2db5b8e3626a8c05c2be548f3d87c41a
Esmyrna <Esmyrnaoliveira13@gmail.com>
2022-12-21 15:44:26 -0300

    movieLibs

---
+++ moviesLib/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import {useState, useEffect} from "react";
+import MovieCard from "../components/MovieCard";
+import './MovieGrid.css'
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
+ 
+const Home = () => {
+    // Top melhores filmes
+    const [topMovies, setTopMovies] = useState([]);
+
+    // Chamar os melhores filmes pela api através da função:
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+        // Preenchimento do set:
+        setTopMovies(data.results);
+
+        
+        };
+        // chamar a função toda vez q a página for carregada
+        useEffect(() => {
+            const  topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+            getTopRatedMovies(topRatedUrl);
+        }, []);
+
+       
+
+        return(
+            <div className="container">
+                <h2 className="title">Melhores filmes</h2>
+                <div className="movies-container">
+                    {topMovies.length === 0 && <p>Carregando</p>}
+                {topMovies.length > 0 && topMovies.map((movie) => 
+                  <MovieCard key={movie.id} movie={movie}/> 
+                )}
+             </div> 
+            </div>
+        ) 
+    
+    }
+
+   
+
+export default Home;
